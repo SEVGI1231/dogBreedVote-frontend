8008101b6a70788ecde6d3d4cd76b710d14ed597
Sevgi <sevgi2308@gmail.com>
2022-09-15 09:30:22 +0300

    styling failed with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import "./App.css";
 function App(): JSX.Element {
   return (
     <>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       <PickDog />
     </>
   );
